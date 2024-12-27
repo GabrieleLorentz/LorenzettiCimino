abb5a7504fad645695416f555cdc145074c1e07b
emanuele cimino <Emanuele.cimino@mail.polimi.it>
2024-12-27 16:12:32 +0100

    Prova

---
+++ S_and_C/src/main/java/com/example/s_and_c/SAndCApplication.java
@@ -0,0 +1,13 @@
+package com.example.s_and_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SAndCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SAndCApplication.class, args);
+    }
+
+}
---
+++ S_and_C/src/test/java/com/example/s_and_c/SAndCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s_and_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SAndCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
