9976fff4b53398447c2cca936c0480e4b1542817
emanuele cimino <Emanuele.cimino@mail.polimi.it>
2025-01-10 15:42:54 +0100

    Adding files

--- S_and_C/src/main/java/com/example/s_and_c/SAndCApplication.java
+++ S_and_C/src/main/java/com/example/s_and_c/SAndCApplication.java
@@ -2,12 +2,21 @@ package com.example.s_and_c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+
+
 public class SAndCApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SAndCApplication.class, args);
     }
+
 
 }
